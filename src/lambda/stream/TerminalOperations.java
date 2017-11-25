74cc77e6fa251a46f84a4da0e9f3f763125c3a17
languno <languno@test.org>
2017-11-25 14:26:37 +0100

    Added mutable reduction with collect().

--- src/lambda/stream/TerminalOperations.java
+++ src/lambda/stream/TerminalOperations.java
@@ -1,6 +1,8 @@
 package lambda.stream;
 
 import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -17,6 +19,7 @@ public class TerminalOperations {
         long c = sup.get().count();
         System.out.println("stream count: " + c);
 
+        System.out.println("# comparator");
         //Comparator<String> comparator = (s1, s2) -> s1.length() - s2.length();
         Comparator<String> comparator = Comparator.comparing(String::length);
         System.out.println("shortest: " + sup.get().min(comparator));
@@ -24,17 +27,21 @@ public class TerminalOperations {
 
         // findAny, get one element, order ignored, useful for parallel streams
         // findFirst, get the first element of the stream
+        System.out.println("# find*");
         System.out.println("findAny: " + sup.get().findAny());
         System.out.println("findFirst: " + sup.get().findFirst());
 
+        System.out.println("# match");
         Predicate<String> startWithLetter = x -> Character.isLetter(x.charAt(0));
         System.out.println("anyMatch starts with letter: " + sup.get().anyMatch(startWithLetter));
         System.out.println("allMatch starts with letter: " + sup.get().allMatch(startWithLetter));
         System.out.println("noneMatch starts with letter: " + sup.get().noneMatch(startWithLetter));
 
+        System.out.println("# forEach");
         sup.get().forEach(System.out::println);
 
         // reduction: concat
+        System.out.println("# reduction");
         Stream<String> wolfStream = Stream.of("w", "o", "l", "f");
         //String word = wolfStream.reduce("", (s, t) -> s + t);
         String word = wolfStream.reduce("", String::concat);
@@ -58,5 +65,19 @@ public class TerminalOperations {
         // with intermediate operations for parallel streams
         Stream<Integer> largeElemIntStream = Stream.of(1, 2, 3, 4, 5, 6); // accumulator gets applied
         System.out.println("reduce with intermediate operation: " + largeElemIntStream.parallel().reduce(1, op, op));
+
+        // collect: the mutable reduction
+        System.out.println("# collect");
+        Supplier<Stream<String>> wolfSupplier = () -> Stream.of("w", "o", "l", "f");
+
+        StringBuilder sb = wolfSupplier.get().collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        System.out.println("StringBuilder: " + sb);
+
+        //TreeSet<String> treeSet = wolfSupplier.get().collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+        TreeSet<String> treeSet = wolfSupplier.get().collect(Collectors.toCollection(TreeSet::new));
+        System.out.println("TreeSet: " + treeSet);
+
+        Set<String> unsortedSet = wolfSupplier.get().collect(Collectors.toSet());
+        System.out.println("Unsorted set: " + unsortedSet);
     }
 }
