d46caf2fa3d6692aa054e5fe06521038563d7229
languno <ernst.tino@gmx.net>
2018-01-01 11:03:36 +0100

    Added mapping of stream to primitive stream.

--- src/lambda/stream/PrimitiveStreams.java
+++ src/lambda/stream/PrimitiveStreams.java
@@ -1,9 +1,11 @@
 package lambda.stream;
 
 import java.util.OptionalDouble;
+import java.util.Random;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class PrimitiveStreams {
     public static void main(String[] args) {
@@ -23,6 +25,22 @@ public class PrimitiveStreams {
         OptionalDouble od = longStream.average();
         System.out.println("double avg: " + od.getAsDouble());
 
+        System.out.println("double randoms");
         doubleStream.limit(3).forEach(System.out::println);
+
+        System.out.println("range 0-5:");
+        IntStream.range(0, 5).forEach(System.out::println);
+
+        System.out.println("rangeClosed 0-5:");
+        IntStream.rangeClosed(0, 5).forEach(System.out::println);
+
+        System.out.println("Random() ints:");
+        new Random().ints().limit(3).forEach(System.out::println);
+
+        // map to primitive streams
+        System.out.println("map stream to primitive stream");
+        Stream<String> stringStream = Stream.of("apple", "orange", "banana");
+        IntStream stringInts = stringStream.peek(System.out::println    ).mapToInt(String::length);
+        stringInts.forEach(System.out::println);
     }
 }
