0ffd4cf2f6ac99539ae3f5e18f407d79b48183b5
languno <ernst.tino@gmx.net>
2018-01-01 10:47:25 +0100

    Improved primitive stream examples.

--- src/lambda/stream/PrimitiveStreams.java
+++ src/lambda/stream/PrimitiveStreams.java
@@ -1,5 +1,6 @@
 package lambda.stream;
 
+import java.util.OptionalDouble;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -11,13 +12,17 @@ public class PrimitiveStreams {
         IntStream intStream = IntStream.iterate(1, x -> x * 2);
 
         // long
-        LongStream longStream = LongStream.of(1L, 2L, 3L);
+        LongStream longStream = LongStream.of(1L, 2L, 3L, 4L);
 
         // double, float
         DoubleStream doubleStream = DoubleStream.generate(Math::random);
 
-        intStream.limit(3).forEach(System.out::println);
-        longStream.limit(3).forEach(System.out::println);
+        int intSum = intStream.limit(3).sum();
+        System.out.println("int sum: " + intSum);
+
+        OptionalDouble od = longStream.average();
+        System.out.println("double avg: " + od.getAsDouble());
+
         doubleStream.limit(3).forEach(System.out::println);
     }
 }
