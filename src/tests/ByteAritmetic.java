2fb8f90b1f8fe83acdde93350bc00ede4a7f697b
languno <ernst.tino@gmx.net>
2018-01-23 16:01:10 +0100

    Improved bitwise operations example.

--- src/tests/ByteAritmetic.java
+++ src/tests/ByteAritmetic.java
@@ -1,19 +1,24 @@
 package tests;
 
+/**
+ * Bitwise and shift operations example.
+ *
+ * @author Tino Ernst
+ */
 public class ByteAritmetic {
 	public static void main(String[] args) {
 		byte a;
 		
 		a = 10;
-		System.out.println(a += 3);
+		System.out.println(a + 3);
 		a = 15;
-		System.out.println(a -= 3);
+		System.out.println(a - 3);
 		a = 20;
-		System.out.println(a *= 3);
+		System.out.println(a * 3);
 		a = 25;
-		System.out.println(a /= 3);
+		System.out.println(a / 3);
 		a = 30;
-		System.out.println(a %= 3);
+		System.out.println(a % 3);
 		a = 35;
 		System.out.println("a = 35");
 		printBinary(a);
@@ -35,33 +40,30 @@ public class ByteAritmetic {
 		a = 50;
 		System.out.println("a = 50 <<");
 		printBinary(a);
-		System.out.println(a <<= 3);
-		printBinary(a);
+		printBinary(a << 3);
 		
 		a = 55;
 		System.out.println("a = 55 >>");
 		printBinary(a);
-		System.out.println(a >>= 3);
+		System.out.println(a >> 3);
 		printBinary(a);
 		
 		a = 60;
 		System.out.println("a = 60 >>>");
 		printBinary(a);
-		System.out.println(a >>>= 3);
+		System.out.println(a >>> 3);
 		printBinary(a);
 		
 		a = -127;
 		System.out.println("a = -127, >>> 3");
-		printBinary(a);
-		System.out.println(a >>>= 3);
-		printBinary(a);
-		
+		printBinary(a);				// 11111111111111111111111110000001
+		printBinary(a >>> 3);		// 00011111111111111111111111110000
+
 		a = -127;
 		System.out.println("a = -127, >> 3");
-		printBinary(a);
-		System.out.println(a >>= 3);
-		printBinary(a);
-		
+		printBinary(a);				// 11111111111111111111111110000001
+		printBinary(a >> 3);		// 11111111111111111111111111110000
+
 		a = 127;
 		System.out.println("a = 127, >>> 3");
 		printBinary(a);
@@ -71,11 +73,21 @@ public class ByteAritmetic {
 		System.out.println("a = 127, >> 3");
 		printBinary(a);
 		printBinary(a >>= 3);
-		
+
+		int i = -1;
+
+		System.out.println("i = -1");
+		printBinary(-1);
+
+		System.out.println("i  = -1 >>> 3");
+		printBinary(i >>> 3);
+
+		System.out.println("i  = -1 >> 3");
+		printBinary(i >> 3);
 	}
-	
-	private static void printBinary(byte b) {		
-		System.out.println(String.format("%8s",
-	            Integer.toBinaryString((b + 256) % 256)).replace(' ', '0'));
+
+	private static void printBinary(int i) {
+		System.out.println(String.format("%32s",
+				Integer.toBinaryString(i)).replace(' ', '0'));
 	}
 }
